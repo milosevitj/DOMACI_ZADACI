3f8b94e3d6bf60b1feaf0c6a01c4b6564e67b437
milosevitj <kacaaa92@gmail.com>
2023-01-04 12:40:47 +0100

    jj

--- projekat2/script.js
+++ projekat2/script.js
@@ -1,64 +1,64 @@
 
 let pitanje1 = {
     tekst: document.getElementById ("pitanje1"),
-    odgovori: [document.getElementById("Albanija"),document.getElementById("Italija"), document.getElementById("Slovenija"),document.getElementById("Makedonija")],
+    odgovori: [document.getElementsByName("pitanje1")],
     indeks: [3]
 }
-console.log(pitanje1.indeks)
-
 
 let pitanje2 = {
-    tekst: "Kako se zove glavni grad Kipra?",
-    odgovori: ["Larnaka", "Nikozija", "Nijedan od navedenih", "Limasol"],
+    tekst: document.getElementById ("pitanje2"),
+    odgovori: [document.getElementsByName("pitanje2")],
     indeks: [1]
 }
 
+
 let pitanje3 = {
-    tekst: "Helsinki je glavni grad...",
-    odgovori:["Finske", "Danske", "Aljaske", "Ukrajine"],
+    tekst: document.getElementById ("pitanje3"),
+    odgovori: [document.getElementsByName("pitanje3")],
     indeks:[0]
 }
 
 let pitanje4 = {
-    tekst: "Poljska se ne graniči sa jednom od navedenih država. Kojom?",
-    odgovori: ["Češka", "Belorusija", "Litvanija", "Austrija"],
+    tekst: document.getElementById ("pitanje4"),
+    odgovori: [document.getElementsByName("pitanje4")],
     indeks: [3]
 }
 
 let pitanje5 = {
-    tekst: "Kom okrugu pripada grad Vranje?",
-    odgovori: ["Pčinjskom", "Rasinskom", "Bačkom", "Nišavskom"],
+    tekst: document.getElementById ("pitanje5"),
+    odgovori: [document.getElementsByName("pitanje5")],
     indeks:[0]
 }
 
 let pitanje6 = {
-    tekst: "U kom glavnom gradu se nalazi 'Zabranjeni grad'?",
-    odgovori: ["Peking", "Džakarta", "Hong Kong", "PjongJang" ],
+    tekst: document.getElementById ("pitanje6"),
+    odgovori: [document.getElementsByName("pitanje6")],
     indeks: [0]
 }
 
 let pitanje7 ={
-    tekst: "Ulan Bator je glavni grad..",
-    odgovori: ["Kambodže", "Avagnistana", "Mongolije", "Jermenije"],
+    tekst: document.getElementById ("pitanje7"),
+    odgovori: [document.getElementsByName("pitanje7")],
     indeks: [2]
 }
 
 let pitanje8 = {
-    tekst: "Pored engleskog, u Pakistanu je sluzbeni jezik..",
-    odgovori:["urdu", "turski", "persijski", "francuski"],
+    tekst: document.getElementById ("pitanje8"),
+    odgovori:[document.getElementsByName("pitanje8")],
     indeks:[0]
 }
 
 let pitanje9 = {
-    tekst: "Koji je pun naziv Mauricijusa?",
-    odgovori: ["Kraljevina Mauricijus", "Republika Mauricijus", "Francuska Republika Mauricijus", "Demokratska Država Mauricijus"],
+    tekst: document.getElementById ("pitanje9"),
+    odgovori: [document.getElementsByName("pitanje9")],
     indeks:[1]
 }
 
 let pitanje10 = {
     tekst: "Koji je glavni grad Gruzije",
     odgovori:["Batumi", "Kutaisi", "Tbilisi", "Gori"],
-    indeks:[2]
+    indeks:[1]
 }
 
 let pitanja=[pitanje1,pitanje2,pitanje3,pitanje4,pitanje5,pitanje6,pitanje7,pitanje8,pitanje9,pitanje10]
+console.log(pitanja)
