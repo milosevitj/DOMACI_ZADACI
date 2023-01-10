e3a6eb14d2d5cd018b48b4afcbfd23356d6ee2da
milosevitj <kacaaa92@gmail.com>
2023-01-10 13:21:55 +0100

    ispravka

--- projekat2_ITJS15013/script.js
+++ projekat2_ITJS15013/script.js
@@ -1,88 +1,83 @@
-let pitanja = [
+let questions = [
   {
-    tekst: "Koja od navedenih država nema izlaz na Jadransko more?",
-    odgovori: ["Albanija", "Italija", "Slovenija", "Makedonija"],
-    indeks: 3,
+    text: "Koja od navedenih država nema izlaz na Jadransko more?",
+    answers: ["Albanija", "Italija", "Slovenija", "Makedonija"],
+    index: 3,
   },
 
   {
-    tekst: "Kako se zove glavni grad Kipra?",
-    odgovori: ["Larnaka", "Nikozija", "Nijedan od navedenih", "Limasol"],
-    indeks: 1,
+    text: "Kako se zove glavni grad Kipra?",
+    answers: ["Larnaka", "Nikozija", "Nijedan od navedenih", "Limasol"],
+    index: 1,
   },
 
   {
-    tekst: "Helsinki je glavni grad...",
-    odgovori: ["Finske", "Danske", "Aljaske", "Ukrajine"],
-    indeks: 0,
+    text: "Helsinki je glavni grad...",
+    answers: ["Finske", "Danske", "Aljaske", "Ukrajine"],
+    index: 0,
   },
 
   {
-    tekst: "Poljska se ne graniči sa jednom od navedenih država. Kojom?",
-    odgovori: ["Češka", "Belorusija", "Litvanija", "Austrija"],
-    indeks: 3,
+    text: "Poljska se ne graniči sa jednom od navedenih država. Kojom?",
+    answers: ["Češka", "Belorusija", "Litvanija", "Austrija"],
+    index: 3,
   },
 
   {
-    tekst: "Kom okrugu pripada grad Vranje?",
-    odgovori: ["Pčinjskom", "Rasinskom", "Bačkom", "Nišavskom"],
-    indeks: 0,
+    text: "Kom okrugu pripada grad Vranje?",
+    answers: ["Pčinjskom", "Rasinskom", "Bačkom", "Nišavskom"],
+    index: 0,
   },
 
   {
-    tekst: "U kom glavnom gradu se nalazi 'Zabranjeni grad'?",
-    odgovori: ["Peking", "Džakarta", "Hong Kong", "PjongJang"],
-    indeks: 0,
+    text: "U kom glavnom gradu se nalazi 'Zabranjeni grad'?",
+    answers: ["Peking", "Džakarta", "Hong Kong", "PjongJang"],
+    index: 0,
   },
 
   {
-    tekst: "Ulan Bator je glavni grad..",
-    odgovori: ["Kambodže", "Avagnistana", "Mongolije", "Jermenije"],
-    indeks: 2,
+    text: "Ulan Bator je glavni grad..",
+    answers: ["Kambodže", "Avagnistana", "Mongolije", "Jermenije"],
+    index: 2,
   },
 
   {
-    tekst: "Pored engleskog, u Pakistanu je sluzbeni jezik..",
-    odgovori: ["urdu", "turski", "persijski", "francuski"],
-    indeks: 0,
+    text: "Pored engleskog, u Pakistanu je sluzbeni jezik..",
+    answers: ["urdu", "turski", "persijski", "francuski"],
+    index: 0,
   },
 
   {
-    tekst: "Koji je pun naziv Mauricijusa?",
-    odgovori: [
-      "Kraljevina Mauricijus",
-      "Republika Mauricijus",
-      "Francuska Republika Mauricijus",
-      "Demokratska Država Mauricijus",
-    ],
-    indeks: 1,
+    text: "Koji je pun naziv Mauricijusa?",
+    answers: ["Kraljevina Mauricijus", "Republika Mauricijus", "Francuska Republika Mauricijus", "Demokratska Država Mauricijus"],
+    index: 1,
   },
 
   {
-    tekst: "Koji je glavni grad Gruzije",
-    odgovori: ["Batumi", "Kutaisi", "Tbilisi", "Gori"],
-    indeks: 2,
+    text: "Koji je glavni grad Gruzije",
+    answers: ["Batumi", "Kutaisi", "Tbilisi", "Gori"],
+    index: 2,
   },
 ];
 
 let bodi = document.querySelector("body");
 bodi.style.marginBottom = "200px";
-let naslov = document.createElement("h1");
-naslov.textContent = "ZANIMLJIVA GEOGRAFIJA";
-bodi.appendChild(naslov);
-let pitanjaOdgovoriElement = document.createElement("div");
-pitanjaOdgovoriElement.setAttribute("id","div2")
-bodi.appendChild(pitanjaOdgovoriElement);
+let title = document.createElement("h1");
+title.textContent = "ZANIMLJIVA GEOGRAFIJA";
+bodi.appendChild(title);
+let questionsanswersElement = document.createElement("div");
+questionsanswersElement.setAttribute("id","div2")
+bodi.appendChild(questionsanswersElement);
 
-function pitanjaOdgovori(nizPitanja) {
-  pitanjaOdgovoriElement.innerHTML = "";
-  //pitanja i odgovori
-  for (let i = 0; i < nizPitanja.length - 5; i++) {
+function questionsanswers(questions) {
+  questionsanswersElement.innerHTML = "";
+  //questions i answers
+  for (let i = 0; i < questions.length - 5; i++) {
     let div = document.createElement("div");
-    let naslov = document.createElement("h4");
-    naslov.textContent = `${i + 1}. ${nizPitanja[i].tekst}`;
-    div.appendChild(naslov);
-    for (let j = 0; j < nizPitanja[i].odgovori.length; j++) {
+    let title = document.createElement("h4");
+    title.textContent = `${i + 1}. ${questions[i].text}`;
+    div.appendChild(title);
+    for (let j = 0; j < questions[i].answers.length; j++) {
       let radio = document.createElement("input");
 
       radio.setAttribute("type", "radio");
@@ -94,62 +89,63 @@ function pitanjaOdgovori(nizPitanja) {
         radio.checked = true;
       }
 
-      let odgovor = document.createElement("label");
-      odgovor.setAttribute("for", j);
-      odgovor.textContent = nizPitanja[i].odgovori[j];
-      radio.appendChild(odgovor);
-      odgovor.innerHTML += `<br>`;
-      div.append(radio, odgovor);
-      pitanjaOdgovoriElement.appendChild(div);
+      let answer = document.createElement("label");
+      answer.setAttribute("for", j);
+      answer.textContent = questions[i].answers[j];
+      radio.appendChild(answer);
+      answer.innerHTML += `<br>`;
+      div.append(radio, answer);
+      questionsanswersElement.appendChild(div);
     }
   }
 }
-pitanjaOdgovori(pitanja);
+questionsanswers(questions);
 
-let dugme = document.createElement("button");
-dugme.textContent = "Pošalji odgovore";
-dugme.setAttribute("id", "odg");
-let dugme2 = document.createElement("button");
-dugme2.textContent = "Nova pitanja";
-dugme2.setAttribute("id", "new");
-bodi.append(dugme, dugme2);
+let butt = document.createElement("button");
+butt.textContent = "Pošalji odgovore";
+butt.setAttribute("id", "odg");
+let butt2 = document.createElement("button");
+butt2.textContent = "Nova pitanja";
+butt2.setAttribute("id", "new");
+bodi.append(butt, butt2);
 
-dugme.addEventListener("click", () => {
-    let sviInputi = document.querySelectorAll("input");
-    for(let k=0;k<sviInputi.length;k++){
-      sviInputi[k].disabled=true
-    }
+
+butt.addEventListener("click", () => {
+  let allInputs = document.querySelectorAll("input");
+  for(let k=0;k<allInputs.length;k++){
+    allInputs[k].disabled=true
+  }
   
-  let prikaziOdgovore = document.createElement("div");
-  prikaziOdgovore.id = "prikaziOdgovore";
+  let sendAnswers = document.createElement("div");
+  sendAnswers.id = "showAnswers";
 
-  let konacniOdgovor = document.querySelectorAll("input:checked");
-
-  for (let i = 0; i < pitanja.length - 5; i++) {
-    let odgovorText = document.createElement("p");
-    if (pitanja[i].indeks == konacniOdgovor[i].id) {
-      odgovorText.textContent = `Tacno ste odgovorili na ${i + 1}. pitanje !!!`;
-      odgovorText.style.color = "green";
-      prikaziOdgovore.appendChild(odgovorText);
+  let trueAnswer = document.querySelectorAll("input:checked");
+  for (let i = 0; i < questions.length - 5; i++) {
+    let answerText = document.createElement("p");
+    if (questions[i].index == trueAnswer[i].id) {
+      answerText.textContent = `Tacno ste odgovorili na ${i + 1}. pitanje !!!`;
+      answerText.style.color = "green";
+      sendAnswers.appendChild(answerText);
     } else {
-      odgovorText.textContent = `Niste tacno ste odgovorili na ${i + 1}. pitanje!!!`;
-      odgovorText.style.color = "red";
-      prikaziOdgovore.appendChild(odgovorText);
+      answerText.textContent = `Niste tacno ste odgovorili na ${i + 1}. pitanje!!!`;
+      answerText.style.color = "red";
+      sendAnswers.appendChild(answerText);
     }
   }
-  bodi.appendChild(prikaziOdgovore);
+  bodi.appendChild(sendAnswers);
 });
 
-let questionsShuffle = function (pitanja) {
-  for (let i = pitanja.length - 1; i > 0; i--) {
-    for (let i = pitanja.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          let temp = pitanja[i];
-          pitanja[i] = pitanja[j];
-          pitanja[j] = temp;
+let questionsShuffle = function (questions) {
+  for (let i = questions.length - 1; i > 0; i--) {
+    for (let i = questions.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = questions[i];
+      questions[i] = questions[j];
+      questions[j] = temp;
+    }
+    questionsanswers(questions)
   }
-  pitanjaOdgovori(pitanja)}
 }
-let noviNiz = questionsShuffle(pitanja)
-dugme2.addEventListener("click", () => { location.reload(); });
+let newOrder = questionsShuffle(questions)
+butt2.addEventListener("click", () => { location.reload(); });
 
