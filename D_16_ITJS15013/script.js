03462d44680d7b7df7bbbf5a3ec10f1109b3071d
milosevitj <kacaaa92@gmail.com>
2023-01-18 19:13:00 +0100

    lala

--- D_16_ITJS15013/script.js
+++ D_16_ITJS15013/script.js
@@ -1,56 +1,48 @@
 let input = document.getElementById("tekst");
 let list = document.querySelector("ul");
 
-let taskElements = document.querySelectorAll(".task");
 let arrTasks = [];
-for (let i = 0; i < taskElements.length; i++) {
-  arrTasks.push(taskElements[i].textContent);
-}
 arrTasks = JSON.parse(localStorage.getItem("arrTasks")) || [];
-localStorage.setItem("arrTasks", JSON.stringify(arrTasks));
 
+for (let i = 0; i < arrTasks.length; i++) {
+  let task = arrTasks[i];
+  let taskItem = document.createElement("li");
+  taskItem.innerHTML = task;
+  list.appendChild(taskItem);
+}
 
 input.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
+  if (e.key == "Enter") {
     let newLiValue = input.value;
     let radio = document.getElementsByName("radio");
     let noviLi = document.createElement("li");
     noviLi.textContent = newLiValue;
-    if (newLiValue !== "") {
+    if (newLiValue != 0) {
       radio.forEach((input) => {
         if (input.checked) {
-          if (input.value === "pocetak") {
+          if (input.value == "pocetak") {
             list.prepend(noviLi);
+            arrTasks.unshift(newLiValue);
           } else {
             list.appendChild(noviLi);
+            arrTasks.push(newLiValue);
           }
         }
       });
-      arrTasks.push(newLiValue);
       localStorage.setItem("arrTasks", JSON.stringify(arrTasks));
       input.value = "";
     }
   }
 });
-
-
+// brisanje jednog  taska 
 list.addEventListener("click", (e) => {
   if (e.target.tagName == "LI") {
-    e.target.remove();
+    let taskToRemove = e.target.textContent;
+    let index = arrTasks.indexOf(taskToRemove);
+    if (index > -1) {
+      arrTasks.splice(index, 1);
+      localStorage.setItem("arrTasks", JSON.stringify(arrTasks));
+      e.target.remove();
+    }
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
