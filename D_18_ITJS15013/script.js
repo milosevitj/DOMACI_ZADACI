3bc8437604d3e9918985f718a620e74dd96a7b7a
milosevitj <kacaaa92@gmail.com>
2023-01-26 12:44:51 +0100

    dodat zadatak

--- D_18_ITJS15013/script.js
+++ D_18_ITJS15013/script.js
@@ -11,18 +11,18 @@ db.collection("movies")
   .doc("movie2")
   .set({
     name: "How to train your dragon",
-    director: "Dean DeBLois",
+    director: { name: "Dean", surname: "DeBlois" },
     release_year: 2010,
     genres: ["Action", "Fantasy", "Drama", "Comedy", "Family film"],
     rating: 10,
   })
   .then(() => {
     console.log("Film2 je uspesno dodat");
-    return db // obrisi zanr
+    return db
       .collection("movies")
       .doc("movie2")
       .update({
-        genres: firebase.firestore.FieldValue.arrayRemove("Comedy"),
+        genres: firebase.firestore.FieldValue.arrayRemove("Comedy"), // obrisi zanr
       });
   })
   .then(() => {
@@ -37,18 +37,18 @@ db.collection("movies")
   .doc("movie3")
   .set({
     name: "Lost in translation",
-    director: "Sofia Copola",
+    director: { name: "Sofia", surname: "Coppola" },
     release_year: 2003,
     genres: ["Romance", "Drama", "Comedy"],
-    rating: 8,
+    rating: 9,
   })
   .then(() => {
     console.log("Film3 je uspesno dodat");
-    return db // dodaj zanr
+    return db
       .collection("movies")
       .doc("movie3")
       .update({
-        genres: firebase.firestore.FieldValue.arrayUnion("Comedy of manners"),
+        genres: firebase.firestore.FieldValue.arrayUnion("Comedy of manners"), // dodaj zanr
       });
   })
   .then(() => {
@@ -57,7 +57,30 @@ db.collection("movies")
   .catch((err) => {
     console.log("greska" + err);
   });
-//promeni
+//movie4
+db.collection("movies")
+  .doc("movie4")
+  .set({
+    name: "Moonrise Kingdom",
+    director: { name: "Wes", surname: "Ander" },
+    release_year: 2012,
+    genres: ["Romance", "Drama", "Comedy"],
+    rating: 8,
+  })
+  .then(() => {
+    console.log("Film4 je uspesno dodat");
+    return db.collection("movies").doc("movie4").update({
+      "director.surname": "Andreson", // promeni prezime
+    });
+  })
+  .then(() => {
+    console.log("Uspeno je promenjeno prezime");
+  })
+  .catch((err) => {
+    console.log("greska" + err);
+  });
+
+//promeni rating prvom filmu
 db.collection("movies")
   .doc("movie1")
   .set(
